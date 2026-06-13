3fadc94244ecd487323575dd5727310d8f356f7e
coralgo <coralgo@post.bgu.ac.il>
2026-06-13 15:40:39 +0300

    edit DB

--- dev/PresentationLayer/Main.java
+++ dev/PresentationLayer/Main.java
@@ -1,17 +1,26 @@
 package PresentationLayer;
 
+import DataAccessLayer.*;
 import ServiceLayer.EmployeeService;
 import ServiceLayer.ShiftService;
 import ServiceLayer.DataInitializer;
+import DomainLayer.HRRepository;
+import DomainLayer.HRRepositoryImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // Step 1: Initialize Services
-        EmployeeService empService = new EmployeeService();
-        ShiftService shiftService = new ShiftService(empService);
 
-        // Initializing with data
-        DataInitializer.seedData(empService, shiftService);
+        try { DataAccessLayer.Database.getConnection().close(); } catch (Exception e) {}
+
+        EmployeeDAO employeeDAO = new JdbcEmployeeDAO();
+        ConstraintDAO constraintDAO = new JdbcConstraintDAO();
+        ShiftDAO shiftDAO = new JdbcShiftDAO();
+
+        HRRepository hrRepository = new HRRepositoryImpl(employeeDAO, constraintDAO, shiftDAO);
+
+        // Step 1: Initialize Services
+        EmployeeService empService = new EmployeeService(hrRepository);
+        ShiftService shiftService = new ShiftService(hrRepository, empService);
 
         // Step 2: Initialize UI with the new name
         UserInterface ui = new UserInterface(empService, shiftService);
