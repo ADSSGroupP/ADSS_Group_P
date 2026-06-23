64033a6b56cd209809a05b6c1ae46ed070d52f9c
noaeliyahou <eliyahon@post.bgu.ac.il>
2026-06-23 17:10:10 +0300

    finished merge between the modules

--- dev/Main.java
+++ dev/Main.java
@@ -1,16 +1,4 @@
 import java.util.Scanner;
-import Presentation.InventoryMenu;
-import Service.DatabaseManager;
-import Service.IntegrationDummyFunctions;
-import Service.InventoryService;
-import devEmployees.DataAccessLayer.JdbcEmployeeDAO;
-import devEmployees.DataAccessLayer.JdbcConstraintDAO;
-import devEmployees.DataAccessLayer.JdbcShiftDAO;
-import devEmployees.DomainLayer.HRRepository;
-import devEmployees.DomainLayer.HRRepositoryImpl;
-import devEmployees.ServiceLayer.EmployeeService;
-import devEmployees.ServiceLayer.ShiftService;
-import devEmployees.PresentationLayer.UserInterface;
 
 /**
  * Main entry point for the Super-Li Supermarket Management System.
@@ -32,30 +20,14 @@ public class Main {
 
         switch (choice) {
             case "1":
-                InventoryService service = new InventoryService();
-                IntegrationDummyFunctions.init(service);
-                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                    DatabaseManager.closeConnection();
-                    System.out.println("System shutdown complete.");
-                }));
-                new InventoryMenu(service).start();
+                devInventory.Presentation.Main.main(args);
                 break;
-
             case "2":
-                JdbcEmployeeDAO employeeDAO = new JdbcEmployeeDAO();
-                JdbcConstraintDAO constraintDAO = new JdbcConstraintDAO();
-                JdbcShiftDAO shiftDAO = new JdbcShiftDAO();
-                HRRepository hrRepository = new HRRepositoryImpl(employeeDAO, constraintDAO, shiftDAO);
-                EmployeeService empService = new EmployeeService(hrRepository);
-                ShiftService shiftService = new ShiftService(hrRepository, empService);
-                UserInterface ui = new UserInterface(empService, shiftService);
-                ui.start();
+                devEmployees.PresentationLayer.Main.main(args);
                 break;
-
             case "3":
                 System.out.println("Goodbye!");
                 break;
-
             default:
                 System.out.println("Invalid choice. Exiting.");
         }
